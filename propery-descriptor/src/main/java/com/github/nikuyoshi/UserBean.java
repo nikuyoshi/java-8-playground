89d77681cd20257e5c8ecd44f6a1144b5fa819e2
Hiroki UCHIDA <nikuyoshi@gmail.com>
2015-11-15 00:09:48 +0900

    Convert from tab separate to space separate

--- propery-descriptor/src/main/java/com/github/nikuyoshi/UserBean.java
+++ propery-descriptor/src/main/java/com/github/nikuyoshi/UserBean.java
@@ -2,13 +2,13 @@ package com.github.nikuyoshi;
 
 public class UserBean {
 
-	private String name;
-	
-	public void setName(String _name){
-		this.name = _name;
-	}
-	
-	public String getName(){
-		return this.name;
-	}
+    private String name;
+    
+    public void setName(String _name){
+        this.name = _name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
 }
